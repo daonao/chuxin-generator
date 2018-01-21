29cb73fb3fd181a26015e4de9c023410736e3984
daonao <daonao.github.com@daonao.com>
2018-01-21 17:12:45 +0800

    gohugo-site/gohugo-generate.sh

---
+++ gohugo-site/gohugo-generate.sh
@@ -0,0 +1,17 @@
+run=1
+while [ $run -gt 0 ]; do
+echo --------------------
+err=$(hugo -D 2>&1 > /dev/null)
+echo "$err"
+run=0
+for line in "$err"
+do
+echo "$line"
+parse_page_metadata=$(echo "$line" | sed -n 's/^Error: Error building site: failed to parse page metadata for "\(.*\)".*/\1/p')
+if [ x"$parse_page_metadata"y != "xy" ]; then
+echo "$parse_page_metadata"
+rm content/"$parse_page_metadata"
+run=1
+fi
+done
+done
