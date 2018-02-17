54dbf05aa2713d8f3da0441abf4d4feb435a11c9
huzhenghui <hu@daonao.com>
2018-02-17 19:10:38 +0800

    delete comment

--- gohugo-site/gohugo-process-stdout.sh
+++ gohugo-site/gohugo-process-stdout.sh
@@ -1,8 +1,3 @@
-# ERROR 2018/02/17 10:09:39 Failed to parse date '2017-9-17 08:10:27' in page posts/Thinkgamer/机器学习/回归分析之理论篇.md
-# ERROR 2018/02/17 10:09:39 Failed to parse date '2016-12-21 00:35' in page posts/Thinkgamer/随手记/别了青春与流年，遇见下一个自己.md
-echo "ERROR 2018/02/17 10:09:39 Failed to parse date '2016-12-21 00:35' in page posts/Thinkgamer/随手记/别了青春与流年，遇见下一个自己.md" |
-sed -n "s/^ERROR [0-9/]\+ [0-9:]\+ Failed to parse date '[0-9-]\+ [0-9:]\+' in page //p"
-
 run=1
 while [ $run -gt 0 ]; do
 echo ----------run----------
